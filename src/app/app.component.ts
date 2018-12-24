856c96cea407b8807cf251a3fe7ebae00a333e37
ramraj666 <pmaurya196@gmail.com>
2018-12-25 00:10:21 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import TodoItem from './TodoItem'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'todolist';
+  todos : TodoItem[] = [];
+
+  ngOnInit() {
+       
+  }
+
+  onNewTodo(newTodo: string){
+    this.todos.push({
+      title: newTodo,
+      isDone: false
+    })
+  }
+
+  onRemoveTodo (index: number){
+    this.todos.splice(index, 1);
+  }
+
 }
---
+++ src/app/todo-form/todo-form.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-form',
+  templateUrl: './todo-form.component.html',
+  styleUrls: ['./todo-form.component.css']
+})
+export class TodoFormComponent implements OnInit {
+
+  newTodo: string = '';
+
+  @Output() onNewTodo = new EventEmitter<string>();
+
+  onSubmit(){
+    console.log(this.newTodo);
+    this.onNewTodo.emit(this.newTodo);
+    this.newTodo = '';
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
